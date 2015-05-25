a22a139bb9d09059c9875ef77c080ef81d9a1d11
Ezekiel Gabrielse <ezekg@yahoo.com>
2015-05-24 19:18:35 -0500

    restructure core files

--- theme-juice/Theme.php
+++ theme-juice/Theme.php
@@ -2,291 +2,84 @@
 
 namespace ThemeJuice;
 
-/**
- * Setup and initialize theme
- *
- * @package Theme Juice Starter
- * @subpackage Theme Juice Core
- * @author Ezekiel Gabrielse, Produce Results
- * @link https://produceresults.com
- * @copyright Produce Results (c) 2014
- * @since 1.0.0
- */
 class Theme {
 
-    /**
-     * @var {String} - String that contains theme root directory
-     */
-    public $root;
+  /**
+   * @var {Array}
+   */
+  private $assets;
 
-    /**
-     * @var {Array} - Array that contains theme assets
-     */
-    public $assets;
+  /**
+   * @var {Array}
+   */
+  private $packages;
 
-    /**
-     * @var {Array} - Array that contains packages to include
-     */
-    public $packages;
+  /**
+   * @var {Array}
+   */
+  private $defaults = array(
+    "assets" => array(),
+    "packages" => array(),
+  );
 
-    /**
-     * @var {Object} - Instance of package \ThemeJuice\Functions,
-     *  used to register theme functions.
-     */
-    public $functions;
+  /**
+   * Constructor
+   *
+   * @param {Array} $options
+   */
+  public function __construct( $options = array() ) {
+    $options = array_merge( self::$defaults, $options );
 
-    /**
-     * @var {Object} - Instance of package \ThemeJuice\Shortcodes,
-     *  used to register theme shortcodes.
-     */
-    public $shortcodes;
+    $this->assets = $options["assets"];
+    $this->packages = $options["packages"];
 
-    /**
-     * @var {Object} - Instance of package \ThemeJuice\Customizer,
-     *  used to setup the theme customizer.
-     */
-    public $customizer;
+    new Loaders\AssetLoader( self::$assets );
+    new Loaders\PackageLoader( self::$packages );
+  }
 
-    /**
-     * Constructor
-     *
-     * @param {Array} $options - Array that contains theme settings
-     */
-    public function __construct( $options = array() ) {
+  /**
+   * Render doctype, head and body tags
+   *
+   * @return {Void}
+   */
+  public function render_head() {
+    do_action( "tj_before_render_head" );
 
-        // Merge new options with defaults
-        $options = array_merge( array(
-            "root" => get_template_directory_uri(),
-            "assets" => array(),
-            "packages" => array(
-                "functions" => array(),
-                "shortcodes" => array(),
-                "customizer" => array(),
-            ),
-        ), $options );
+    $buffer = array();
+    $buffer[] = "<!doctype html>";
+    $buffer[] = "<html class='no-js'>";
+    $buffer[] = "<head>";
+    $buffer[] = "<meta charset='" . get_bloginfo( "charset" ) . "'>";
+    $buffer[] = "<meta http-equiv='x-ua-compatible' content='ie=edge' />";
+    $buffer[] = "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+    $buffer[] = "<link rel='shortcut icon' href='" . get_template_directory_uri() . "/favicon.ico' />";
+    echo implode( "", $buffer );
 
-        // Root directory
-        $this->root = $options["root"];
+    wp_head();
 
-        // Assets to register
-        $this->assets = $options["assets"];
+    $buffer = array();
+    $buffer[] = "</head>";
+    $buffer[] = "<body class='" . implode( " ", get_body_class() ) . "'>";
+    echo implode( "", $buffer );
 
-        // Packages to include
-        $this->packages = $options["packages"];
+    do_action( "tj_after_render_head" );
+  }
 
-        // Fix for PHP <= 5.3.x not allowing $this inside of closures
-        $self = $this;
+  /**
+   * Render footer
+   *
+   * @return {Void}
+   */
+  public function render_footer() {
+    do_action( "tj_before_render_footer" );
 
-        // Lets not load this stuff on admin pages
-        if ( ! $this->on_admin_pages() ) {
+    wp_footer();
 
-            // Add assets
-            if ( ! empty( $this->assets ) ) {
-                add_action( "init", function() use ( &$self ) {
-                    foreach ( $self->assets as $handle => $opts ) {
-                        $self->register_asset( $handle, $opts );
-                    }
-                });
-            }
-        }
+    $buffer = array();
+    $buffer[] = "</body>";
+    $buffer[] = "</html>";
+    echo implode( "", $buffer );
 
-        // Loop through and setup packages
-        if ( $this->packages !== false ) {
-
-            // Filter out disabled packages
-            $this->setup_packages( array_filter( $this->packages, function( $package ) {
-                return $package !== false;
-            }));
-        }
-    }
-
-    /**
-     * Make sure we're not on admin or login pages
-     *
-     * @return {Bool}
-     */
-    public function on_admin_pages() {
-        return ( is_admin() || $GLOBALS["pagenow"] === "wp-login.php" );
-    }
-
-    /**
-     * Setup included packages if available
-     *
-     * @param {Array} $packages - Array of packages and their settings
-     *
-     * @return {Void}
-     */
-    public function setup_packages( $packages ) {
-
-        // Import packages if available, discard disabled packages
-        foreach ( array_keys( $packages ) as $package ) {
-
-            // Create class name
-            $class_name = ucfirst( $package );
-
-            // Import functions
-            if ( class_exists( $PackageClass = "\\ThemeJuice\\$class_name" ) ) {
-
-                // If functions array was passed, then pass in options to constructor
-                $this->{$package} = new $PackageClass( function() use ( $packages, $package ) {
-                    if ( ! empty( $packages[$package] ) ) {
-                        return $packages[$package];
-                    } else {
-                        return array();
-                    }
-                });
-            } else {
-                // @TODO - Show a dialog box in WP backend that links to all of the add-ons
-                //  for Theme Juice, such as functions, shortcodes and the customizer.
-            }
-        }
-    }
-
-    /**
-     * Register asset to theme with 'wp_enqueue_scripts()'
-     *
-     * @param {String} $handle               - The name of the script to register
-     * @param {Array}  $opts                 - Array of options for the script
-     * @param {String} $opts["type"]         - Type of asset to register
-     * @param {Bool}   $opts["external"]     - Use external (off-site) asset
-     * @param {String} $opts["location"]     - Location of asset (relative to $root if not external)
-     * @param {Array}  $opts["dependencies"] - Assets that this asset depends on (i.e. jquery, etc.)
-     * @param {String} $opts["version"]      - Version number for asset
-     * @param {String} $opts["media"]        - Media rule for stylsheets
-     * @param {Bool}   $opts["in_footer"]    - Output script to footer
-     *
-     * @return {Void}
-     */
-    public function register_asset( $handle, $opts ) {
-
-        // Make sure asset type was passed
-        if ( ! isset( $opts["type"] ) ) {
-            throw new \Exception( "Attempted to register asset '$handle' without a type. Aborting mission." );
-        }
-
-        // Make sure asset location was passed
-        if ( ! isset( $opts["location"] ) ) {
-            throw new \Exception( "Attempted to register asset '$handle' without a location. Aborting mission." );
-        } else {
-
-            /**
-             * Make sure this is not an external asset,
-             *   else redefine location from $root
-             */
-            if ( ! isset( $opts["external"] ) || ! $opts["external"] ) {
-                $opts["location"] = $this->root . "/" . $opts["location"];
-            }
-        }
-
-        // Set 'dependencies' option if not set
-        if ( ! isset( $opts["dependencies"] ) ) {
-            $opts["dependencies"] = array();
-        }
-
-        // Set 'version' option if not set
-        if ( ! isset( $opts["version"] ) ) {
-            $opts["version"] = false;
-        }
-
-        switch ( $opts["type"] ) {
-            case "style":
-
-                // Set 'media' option if not set
-                if ( ! isset( $opts["media"] ) ) {
-                    $opts["media"] = "all";
-                }
-
-                // Make sure stylesheet is not already enqueued, if it is,
-                //  deregister it to make room for new stylesheet
-                if ( wp_style_is( $handle, "registered" ) ) {
-                    wp_deregister_style( $handle );
-                }
-
-                // Enqueue stylesheet within closure
-                add_action( "wp_enqueue_scripts", function() use ( $handle, $opts ) {
-                    wp_enqueue_style( $handle, $opts["location"], $opts["dependencies"], $opts["version"], $opts["media"] );
-                }, 50 );
-
-                break;
-            case "script":
-
-                // Set 'in_footer' option if not set
-                if ( ! isset( $opts["in_footer"] ) ) {
-                    $opts["in_footer"] = false;
-                }
-
-                // Make sure script is not already enqueued, if it is,
-                //  deregister it to make room for new script
-                if ( wp_script_is( $handle, "registered" ) ) {
-                    wp_deregister_script( $handle );
-                }
-
-                // Enqueue script within closure
-                add_action( "wp_enqueue_scripts", function() use ( $handle, $opts ) {
-                    wp_enqueue_script( $handle, $opts["location"], $opts["dependencies"], $opts["version"], $opts["in_footer"] );
-                }, 50 );
-
-                break;
-            default:
-                throw new \Exception( "Invalid asset type '" . $opts['type'] . "' for '$handle'. Aborting mission." );
-                break;
-        }
-    }
-
-    /**
-     * Render HTML doctype and head, 'wp_head', opening tags
-     *
-     * @return {Void}
-     */
-    public function render_head() {
-        $buffer = array();
-
-        // Doctype
-        $buffer[] = "<!DOCTYPE html>";
-        $buffer[] = "<html class='no-js'>";
-        $buffer[] = "<head>";
-
-        // Favicon
-        $buffer[] = "<link rel='shortcut icon' href='" . get_template_directory_uri() . "/favicon.ico' />";
-
-        // Required meta tags
-        $buffer[] = "<meta charset='" . get_bloginfo( "charset" ) . "'>";
-        $buffer[] = "<meta http-equiv='X-UA-Compatible' content='IE=edge, chrome=1' />";
-        $buffer[] = "<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>";
-
-        // Return current buffer
-        echo implode( "", $buffer );
-
-        // Wordpress hook
-        wp_head();
-
-        // Create new buffer
-        $buffer = array();
-
-        // Close head and open body
-        $buffer[] = "</head>";
-        $buffer[] = "<body class='" . implode( " ", get_body_class() ) . "'>";
-
-        // Return current buffer
-        echo implode( "", $buffer );
-    }
-
-    /**
-     * Render 'wp_footer', close out tags
-     *
-     * @return {Void}
-     */
-    public function render_footer() {
-        $buffer = array();
-
-        // Wordpress hook
-        wp_footer();
-
-        // Render close body and html
-        $buffer[] = "</body>";
-        $buffer[] = "</html>";
-
-        // Return current buffer
-        echo implode( "", $buffer );
-    }
+    do_action( "tj_after_render_footer" );
+  }
 }
